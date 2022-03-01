7e8628e93a8ff6243503a8f3b4c26a6eb455004e
ClaraSimonsson <clara.simonsson@hotmail.se>
2022-03-01 17:17:15 +0100

    Credits

--- Revenge of the Snorks/Assets/Memory/GameControllerScript.cs
+++ Revenge of the Snorks/Assets/Memory/GameControllerScript.cs
@@ -1,3 +1,11 @@
+/*
+ * The code and some of the picture is taken from the video below
+ * https://www.youtube.com/watch?v=yjQT0IEFbgc&ab_channel=GetWiseTuts 
+ * 
+ * The pictures used in the minigame
+ * https://henrysoftware.itch.io/pixel-food 
+ * 
+ */
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
